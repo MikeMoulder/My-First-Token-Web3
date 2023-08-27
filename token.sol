3f64220868d9056ff961b6a1a485a9bf678b75e4
Michael Ogundeji - Mike Moulder <121914655+MikeMoulder@users.noreply.github.com>
2023-08-27 18:57:35 +0100

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+
+  constructor(uint256 initialSupply) ERC20("MoulderToken", "MTK") {
+    _mint(msg.sender, initialSupply * 10 **18);
+  }
+}
